9b41875e0b3f4c6d4dc7d97f7098aeeb3375f3b0
shymmy <shymmywgarcia@gmail.com>
2023-03-10 19:03:07 -0500

    add something

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,18 @@
 
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
 const errorHandler = require('../global/errorHandling/middleware/errorHandler');
 const app = express();
 
 // Global middlewares
+app.use(helmet());
+app.use(helmet.frameguard());
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api', require('../tasks/tasks.routes'));
