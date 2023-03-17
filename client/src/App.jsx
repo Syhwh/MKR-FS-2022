a7e56e0e92fc5a2f000068b41bf7704044f491c9
shymmy <shymmywgarcia@gmail.com>
2023-03-17 19:08:16 -0400

    add upload images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "./layouts";
+import { Upload } from "./widgets/images/upload";
 
 function App() {
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/auth/*" element={<Auth />} />
+      <Route path="/images/*" element={<Upload />} />
       <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
     </Routes>
   );
